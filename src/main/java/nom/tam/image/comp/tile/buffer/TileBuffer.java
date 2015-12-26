f820bff39c5562e8c2de08212ed54fa1d0e7255d
ritchie <ritchie@gmx.at>
2015-12-26 07:45:09 +0100

    bug found compressed data offset calculation was wrong #48

--- src/main/java/nom/tam/image/comp/tile/buffer/TileBuffer.java
+++ src/main/java/nom/tam/image/comp/tile/buffer/TileBuffer.java
@@ -7,12 +7,12 @@ package nom.tam.image.comp.tile.buffer;
  * Copyright (C) 1996 - 2015 nom-tam-fits
  * %%
  * This is free and unencumbered software released into the public domain.
- * 
+ *
  * Anyone is free to copy, modify, publish, use, compile, sell, or
  * distribute this software, either in source code form or as a compiled
  * binary, for any purpose, commercial or non-commercial, and by any
  * means.
- * 
+ *
  * In jurisdictions that recognize copyright laws, the author or authors
  * of this software dedicate any and all copyright interest in the
  * software to the public domain. We make this dedication for the benefit
@@ -20,7 +20,7 @@ package nom.tam.image.comp.tile.buffer;
  * successors. We intend this dedication to be an overt act of
  * relinquishment in perpetuity of all present and future rights to this
  * software under copyright law.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -84,6 +84,10 @@ public abstract class TileBuffer {
 
     protected Buffer getImageBuffer() {
         return this.imageBuffer;
+    }
+
+    public int getOffset() {
+        return this.offset;
     }
 
     /**
