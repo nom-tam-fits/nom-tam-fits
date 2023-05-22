d10515644c4042b91a2cc5f99d73abae16c1d1cc
Attila Kovacs <attipaci@gmail.com>
2023-05-22 18:01:30 +0200

    Added Fits(FitsFile) constructor and more tests

--- src/test/java/nom/tam/fits/test/BaseFitsTest.java
+++ src/test/java/nom/tam/fits/test/BaseFitsTest.java
@@ -1,37 +1,5 @@
 package nom.tam.fits.test;
 
-/*
- * #%L
- * nom.tam FITS library
- * %%
- * Copyright (C) 1996 - 2021 nom-tam-fits
- * %%
- * This is free and unencumbered software released into the public domain.
- * 
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * #L%
- */
-
-import static nom.tam.fits.header.Standard.NAXISn;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -96,6 +64,39 @@ import nom.tam.util.LoggerHelper;
 import nom.tam.util.RandomAccessFileIO;
 import nom.tam.util.SafeClose;
 import nom.tam.util.test.ThrowAnyException;
+
+/*
+ * #%L
+ * nom.tam FITS library
+ * %%
+ * Copyright (C) 1996 - 2021 nom-tam-fits
+ * %%
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * #L%
+ */
+
+import static nom.tam.fits.header.Standard.NAXISn;
 
 public class BaseFitsTest {
 
@@ -726,8 +727,7 @@ public class BaseFitsTest {
         Assert.assertNotNull(actual);
         actual = null;
         try {
-            new Fits(new URL(FILE + new File("src/test/resources/nom/tam/fits/test/test.fitsX").getAbsolutePath()),
-                    false);
+            new Fits(new URL(FILE + new File("src/test/resources/nom/tam/fits/test/test.fitsX").getAbsolutePath()), false);
         } catch (FitsException ex) {
             actual = ex;
         }
@@ -899,7 +899,8 @@ public class BaseFitsTest {
             expected = e;
         }
         Assert.assertNotNull(expected);
-        // AK: There is no good reason why reset should fail, as the contract of seek() allows
+        // AK: There is no good reason why reset should fail, as the contract of
+        // seek() allows
         // going beyond the end of file...
         // Assert.assertFalse(data.reset());
     }
@@ -963,8 +964,8 @@ public class BaseFitsTest {
 
     @Test(expected = FitsException.class)
     public void testFitsWriteException1() throws Exception {
-        DataOutput out = (DataOutput) Proxy.newProxyInstance(getClass().getClassLoader(),
-                new Class[] {DataOutput.class}, new InvocationHandler() {
+        DataOutput out = (DataOutput) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {DataOutput.class},
+                new InvocationHandler() {
 
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -1293,8 +1294,7 @@ public class BaseFitsTest {
 
     @Test
     public void resetNonRandomAccess() throws Exception {
-        Fits fits = new Fits(
-                new FitsInputStream(new FileInputStream("src/test/resources/nom/tam/fits/test/test.fits")));
+        Fits fits = new Fits(new FitsInputStream(new FileInputStream("src/test/resources/nom/tam/fits/test/test.fits")));
         fits.read();
         assertFalse(fits.getHDU(0).getData().reset());
     }
@@ -1323,6 +1323,11 @@ public class BaseFitsTest {
     @Test(expected = FitsException.class)
     public void repositionFailTest() throws Exception {
         FitsUtil.reposition(new FitsFile("src/test/resources/nom/tam/fits/test/test.fits", "rw"), -1);
+    }
+
+    @Test(expected = FitsException.class)
+    public void createFitsFileExceptionTest() throws Exception {
+        new Fits((FitsFile) null);
     }
 
     private static class TestRandomAccessFileIO extends java.io.RandomAccessFile implements RandomAccessFileIO {
