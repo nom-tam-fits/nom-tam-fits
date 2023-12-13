be8a85688b7f8164739e4fb22e7f7c4ad9771bc6
Attila Kovacs <attipaci@gmail.com>
2023-12-13 20:56:05 +0100

    Revert "Demote FitsException to a runtime exception based on IllegalStateException"
    This reverts commit a46ccda1022c2ee2e2fbface01de3b5d15ac6d92.

--- src/main/java/nom/tam/fits/FitsException.java
+++ src/main/java/nom/tam/fits/FitsException.java
@@ -32,11 +32,12 @@ package nom.tam.fits;
  */
 
 /**
- * When we cannot deal with some FITS data as expected. Originally it was a hard exception, that you had no choice by to
- * catch. Since 1.19, it has been demoted to a softer, runtime exception. This is a back compatible change, which gives
- * more freedom to programmers on dealing with these (or not).
+ * A hard exception for when we cannot deal with some FITS data as expected. In
+ * retrospect it would have been better to make this a softer runtime exception,
+ * but this goes back to the beginning of this library so it is here to stay.
+ * You have no choice but to catch these an deal with them all the time.
  */
-public class FitsException extends IllegalStateException {
+public class FitsException extends Exception {
 
     /**
      *
@@ -46,18 +47,24 @@ public class FitsException extends IllegalStateException {
     /**
      * Instantiates this exception with the designated message string.
      * 
-     * @param msg a human readable message that describes what in fact caused the exception
+     * @param msg
+     *            a human readable message that describes what in fact caused
+     *            the exception
      */
     public FitsException(String msg) {
         super(msg);
     }
 
     /**
-     * Instantiates this exception with the designated message string, when it was triggered by some other type of
-     * exception
+     * Instantiates this exception with the designated message string, when it
+     * was triggered by some other type of exception
      * 
-     * @param msg    a human readable message that describes what in fact caused the exception
-     * @param reason the original exception (or other throwable) that triggered this exception.
+     * @param msg
+     *            a human readable message that describes what in fact caused
+     *            the exception
+     * @param reason
+     *            the original exception (or other throwable) that triggered
+     *            this exception.
      */
     public FitsException(String msg, Throwable reason) {
         super(msg, reason);
