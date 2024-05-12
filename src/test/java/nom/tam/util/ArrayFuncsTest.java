2b487d567069ac59f561dd328ee46af519639442
Attila Kovacs <attipaci@gmail.com>
2024-05-12 17:13:41 +0200

    More tests

--- src/test/java/nom/tam/util/ArrayFuncsTest.java
+++ src/test/java/nom/tam/util/ArrayFuncsTest.java
@@ -333,6 +333,21 @@ public class ArrayFuncsTest {
     }
 
     @Test
+    public void sliceSubReverseTest() throws Exception {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int[][] sub = (int[][]) ArrayFuncs.slice(array, new int[] {1}, new int[] {-2});
+
+        Assert.assertEquals(2, sub.length);
+        Assert.assertEquals(3, sub[0].length);
+
+        Assert.assertEquals(4, sub[0][0]);
+        Assert.assertEquals(5, sub[0][1]);
+        Assert.assertEquals(1, sub[1][0]);
+        Assert.assertEquals(2, sub[1][1]);
+    }
+
+    @Test
     public void reverseSliceTest() throws Exception {
         int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
